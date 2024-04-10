c6d8bb6652541465d970c2bb05df892355c1b67e
maissa SOUISSI <maissa.souissi@rte-france.com>
2024-04-10 13:43:25 +0200

    add tests for properties expert Rule
    Signed-off-by: maissa SOUISSI <maissa.souissi@rte-france.com>

---
+++ src/test/java/org/gridsuite/filter/expertfilter/PropertiesExpertRuleTest.java
@@ -0,0 +1,115 @@
+package org.gridsuite.filter.expertfilter;
+
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.iidm.network.*;
+import org.gridsuite.filter.FilterLoader;
+import org.gridsuite.filter.expertfilter.expertrule.PropertiesExpertRule;
+import org.gridsuite.filter.utils.expertfilter.FieldType;
+import org.gridsuite.filter.utils.expertfilter.OperatorType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.gridsuite.filter.utils.expertfilter.OperatorType.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PropertiesExpertRuleTest {
+    private FilterLoader filterLoader;
+
+    static Stream<Arguments> provideArgumentsForTestWithException() {
+
+        Network network = Mockito.mock(Network.class);
+        Mockito.when(network.getType()).thenReturn(IdentifiableType.NETWORK);
+
+        Substation substation = Mockito.mock(Substation.class);
+        Mockito.when(substation.getProperty("cvgRegion")).thenReturn("LILLE");
+
+        VoltageLevel voltageLevel = Mockito.mock(VoltageLevel.class);
+        Mockito.when(voltageLevel.getType()).thenReturn(IdentifiableType.VOLTAGE_LEVEL);
+        Terminal terminal = Mockito.mock(Terminal.class);
+
+        Mockito.when(terminal.getVoltageLevel()).thenReturn(voltageLevel);
+        Mockito.when(voltageLevel.getNullableSubstation()).thenReturn(substation);
+
+        Line line = Mockito.mock(Line.class);
+        Mockito.when(line.getType()).thenReturn(IdentifiableType.LINE);
+        Terminal terminal1 = Mockito.mock(Terminal.class);
+        Mockito.when(terminal1.getVoltageLevel()).thenReturn(voltageLevel);
+
+        Generator generator = Mockito.mock(Generator.class);
+        Mockito.when(generator.getType()).thenReturn(IdentifiableType.GENERATOR);
+        Mockito.when(generator.getId()).thenReturn("GEN");
+        Mockito.when(generator.getProperty("codeOI")).thenReturn("33");
+        Mockito.when(generator.getProperty("north")).thenReturn("north");
+        Mockito.when(generator.getTerminal()).thenReturn(terminal);
+        Mockito.when(generator.getTerminal().getVoltageLevel().getNullableSubstation().getProperty("cvgRegion")).thenReturn("LILLE");
+
+        Load load = Mockito.mock(Load.class);
+        Mockito.when(load.getType()).thenReturn(IdentifiableType.LOAD);
+
+        ShuntCompensator shuntCompensator = Mockito.mock(ShuntCompensator.class);
+        Mockito.when(shuntCompensator.getType()).thenReturn(IdentifiableType.SHUNT_COMPENSATOR);
+
+        Battery battery = Mockito.mock(Battery.class);
+        Mockito.when(battery.getType()).thenReturn(IdentifiableType.BATTERY);
+
+        TwoWindingsTransformer twoWindingsTransformer = Mockito.mock(TwoWindingsTransformer.class);
+        Mockito.when(twoWindingsTransformer.getType()).thenReturn(IdentifiableType.TWO_WINDINGS_TRANSFORMER);
+
+        return Stream.of(
+                // --- Test an unsupported field for some equipment --- //
+                Arguments.of(EQUALS, FieldType.SUBSTATION_PROPERTIES_1, voltageLevel, "region", List.of("east"), PowsyblException.class),
+                Arguments.of(EQUALS, FieldType.SUBSTATION_PROPERTIES_2, generator, "region", List.of("north"), PowsyblException.class),
+                Arguments.of(EQUALS, FieldType.SUBSTATION_PROPERTIES_1, load, "region", List.of("north"), PowsyblException.class),
+                Arguments.of(EQUALS, FieldType.SUBSTATION_PROPERTIES_1, shuntCompensator, "region", List.of("east"), PowsyblException.class),
+                Arguments.of(EQUALS, FieldType.SUBSTATION_PROPERTIES, line, "region", List.of("east"), PowsyblException.class),
+                Arguments.of(EQUALS, FieldType.SUBSTATION_PROPERTIES_2, battery, "region", List.of("east"), PowsyblException.class),
+
+                // --- Test an unsupported operator for this rule type --- //
+                Arguments.of(IS, FieldType.FREE_PROPERTIES, generator, "codeOI", List.of("33"), PowsyblException.class),
+                Arguments.of(CONTAINS, FieldType.SUBSTATION_PROPERTIES, generator, "cvgRegion", List.of("LILLE"), PowsyblException.class)
+        );
+    }
+
+    private static Stream<Arguments> provideArgumentsForSubstationTest() {
+
+        Substation substation = Mockito.mock(Substation.class);
+        Mockito.when(substation.getType()).thenReturn(IdentifiableType.SUBSTATION);
+        Mockito.when(substation.getProperty("cvgRegion")).thenReturn("LILLE");
+
+        return Stream.of(
+                // --- EQUALS --- //
+                Arguments.of(EQUALS, FieldType.FREE_PROPERTIES, "cvgRegion", List.of("LILLE", "PARIS"), substation, true)
+        );
+    }
+
+    @BeforeEach
+    public void setUp() {
+        filterLoader = uuids -> null;
+    }
+
+    @ParameterizedTest
+    @MethodSource({
+        "provideArgumentsForTestWithException"
+    })
+    void testEvaluateRuleWithException(OperatorType operator, FieldType field, Identifiable<?> equipment, String propertyName, List<String> propertyValues, Class expectedException) {
+        PropertiesExpertRule rule = PropertiesExpertRule.builder().operator(operator).field(field).propertyName(propertyName).propertyValues(propertyValues).build();
+        assertThrows(expectedException, () -> rule.evaluateRule(equipment, filterLoader, new HashMap<>()));
+    }
+
+    @ParameterizedTest
+    @MethodSource({
+        "provideArgumentsForSubstationTest"
+    })
+    void testEvaluateRule(OperatorType operator, FieldType field, String propertyName, List<String> propertyValues, Identifiable<?> equipment, boolean expected) {
+        PropertiesExpertRule rule = PropertiesExpertRule.builder().operator(operator).field(field).propertyName(propertyName).propertyValues(propertyValues).build();
+        assertEquals(expected, rule.evaluateRule(equipment, filterLoader, new HashMap<>()));
+    }
+}
