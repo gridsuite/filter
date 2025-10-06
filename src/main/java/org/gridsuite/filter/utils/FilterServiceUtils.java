f75fb08fc8cdc6fedcb9a3595e4c8a13d1eca5fe
Florent MILLOT <florent.millot_externe@rte-france.com>
2025-10-06 15:23:23 +0200

    Refactor `getIdentifiableAttributes` logic for clarity and add filtering by `IdentifiableType`.
    Signed-off-by: Florent MILLOT <florent.millot_externe@rte-france.com>

--- src/main/java/org/gridsuite/filter/utils/FilterServiceUtils.java
+++ src/main/java/org/gridsuite/filter/utils/FilterServiceUtils.java
@@ -6,14 +6,16 @@
  */
 package org.gridsuite.filter.utils;
 
+import com.powsybl.iidm.network.IdentifiableType;
 import com.powsybl.iidm.network.Network;
-import org.gridsuite.filter.FilterLoader;
 import org.gridsuite.filter.AbstractFilter;
+import org.gridsuite.filter.FilterLoader;
 import org.gridsuite.filter.identifierlistfilter.FilterEquipments;
 import org.gridsuite.filter.identifierlistfilter.IdentifiableAttributes;
 import org.gridsuite.filter.identifierlistfilter.IdentifierListFilter;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -25,20 +27,20 @@ public final class FilterServiceUtils {
     }
 
     public static List<IdentifiableAttributes> getIdentifiableAttributes(AbstractFilter filter, Network network, FilterLoader filterLoader) {
-        if (filter instanceof IdentifierListFilter identifierListFilter &&
-            (filter.getEquipmentType() == EquipmentType.GENERATOR ||
-                filter.getEquipmentType() == EquipmentType.LOAD)) {
-            return FiltersUtils.getIdentifiables(filter, network, filterLoader)
-                .stream()
-                .map(identifiable -> new IdentifiableAttributes(identifiable.getId(),
-                    identifiable.getType(),
-                    identifierListFilter.getDistributionKey(identifiable.getId())))
-                .toList();
-        } else {
-            return FiltersUtils.getIdentifiables(filter, network, filterLoader).stream()
-                .map(identifiable -> new IdentifiableAttributes(identifiable.getId(), identifiable.getType(), null))
-                .toList();
-        }
+        boolean isGeneratorOrLoad = filter instanceof IdentifierListFilter
+            && (filter.getEquipmentType() == EquipmentType.GENERATOR || filter.getEquipmentType() == EquipmentType.LOAD);
+
+        return FiltersUtils.getIdentifiables(filter, network, filterLoader)
+            .stream()
+            .map(identifiable -> new IdentifiableAttributes(identifiable.getId(),
+                identifiable.getType(),
+                isGeneratorOrLoad ? ((IdentifierListFilter) filter).getDistributionKey(identifiable.getId()) : null))
+            .toList();
+    }
+
+    public static List<IdentifiableAttributes> getIdentifiableAttributes(AbstractFilter filter, Set<IdentifiableType> equipmentTypes, Network network, FilterLoader filterLoader) {
+        return FilterServiceUtils.getIdentifiableAttributes(filter, network, filterLoader).stream()
+            .filter(identifiableAttributes -> equipmentTypes.contains(identifiableAttributes.getType())).toList();
     }
 
     public static List<FilterEquipments> getFilterEquipmentsFromUuid(Network network, UUID uuid, FilterLoader filterLoader) {
