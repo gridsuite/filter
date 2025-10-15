2373b20aa9718bceb980c8996d9e1ae43e2ed64f
David BRAQUART <david.braquart@rte-france.com>
2025-10-15 18:50:06 +0200

    simplify cause genericFilters always null
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/main/java/org/gridsuite/filter/globalfilter/AbstractGlobalFilterService.java
+++ src/main/java/org/gridsuite/filter/globalfilter/AbstractGlobalFilterService.java
@@ -2,12 +2,9 @@ package org.gridsuite.filter.globalfilter;
 
 import com.powsybl.iidm.network.Network;
 import lombok.NonNull;
-import org.apache.commons.lang3.ObjectUtils;
-import org.gridsuite.filter.AbstractFilter;
 import org.gridsuite.filter.FilterLoader;
 import org.gridsuite.filter.utils.EquipmentType;
 
-import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
@@ -15,27 +12,18 @@ import java.util.UUID;
 public abstract class AbstractGlobalFilterService implements FilterLoader {
     protected abstract Network getNetwork(@NonNull UUID networkUuid, @NonNull String variantId);
 
-    /** @see #getFilteredIds(UUID, String, GlobalFilter, List) */
-    protected List<String> getFilteredIds(@NonNull final UUID networkUuid, @NonNull final String variantId,
-                                          @NonNull final GlobalFilter globalFilter, @NonNull final List<EquipmentType> equipmentTypes) {
-        return getFilteredIds(networkUuid, variantId, globalFilter, equipmentTypes, null);
-    }
-
     /**
      * Get filtered equipment IDs.
      * @param networkUuid the network to load
      * @param variantId the network variant to work on
      * @param globalFilter the filter(s) to apply
      * @param equipmentTypes the {@link EquipmentType equipment types} to filter
-     * @param genericFilters additional generic filters to apply
      * @return the {@link List list} of {@link UUID IDs} of filtered {@link EquipmentType equipments}.
      */
     protected List<String> getFilteredIds(@NonNull final UUID networkUuid, @NonNull final String variantId,
-                                          @NonNull final GlobalFilter globalFilter, @NonNull final List<EquipmentType> equipmentTypes,
-                                          @Nullable final List<AbstractFilter> genericFilters) {
+                                          @NonNull final GlobalFilter globalFilter, @NonNull final List<EquipmentType> equipmentTypes) {
         final Network network = getNetwork(networkUuid, variantId);
-        return GlobalFilterUtils.applyGlobalFilterOnNetwork(network, globalFilter, ObjectUtils.getIfNull(genericFilters, List::of), equipmentTypes, this)
-            // Filter equipments by type
+        return GlobalFilterUtils.applyGlobalFilterOnNetwork(network, globalFilter, List.of(), equipmentTypes, this)
             .values()
             .stream()
             .filter(Objects::nonNull)
