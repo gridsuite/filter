6f8eca37fa0be8b223ead1db5b079999b07585ce
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2025-10-21 12:30:22 +0200

    Fix javadoc pb and missing copyright (#91)
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/filter/utils/UuidUtils.java
+++ src/main/java/org/gridsuite/filter/utils/UuidUtils.java
@@ -1,7 +1,12 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.filter.utils;
 
 import com.google.common.annotations.VisibleForTesting;
-import lombok.Setter;
 
 import java.util.UUID;
 import java.util.function.Supplier;
@@ -15,7 +20,6 @@ public final class UuidUtils {
         throw new AssertionError("Utility class should not be instantiated");
     }
 
-    @Setter(onMethod_ = {@VisibleForTesting})
     private static Supplier<UUID> uuidSupplier = UUID::randomUUID;
 
     public static UUID generateUUID() {
