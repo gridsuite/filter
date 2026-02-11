cc3124be82b598a012d1c13eb804974d0ab2de53
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-02-11 09:27:51 +0100

    fix: author and copyright
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/test/java/org/gridsuite/filter/utils/FilterServiceUtilsTest.java
+++ src/test/java/org/gridsuite/filter/utils/FilterServiceUtilsTest.java
@@ -1,3 +1,9 @@
+/*
+  Copyright (c) 2026, RTE (http://www.rte-france.com)
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.filter.utils;
 
 import com.powsybl.iidm.network.IdentifiableType;
@@ -20,6 +26,9 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * @author Kevin Le Saulnier <kevin.lesaulnier at rte-france.com>
+ */
 class FilterServiceUtilsTest {
 
     @Test
