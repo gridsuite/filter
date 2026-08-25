66ab405f3c64aed26901b10add72ad5e173b65d6
Joris Mancini <joris.mancini_externe@rte-france.com>
2026-08-25 15:37:19 +0200

    fix copyright and author
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- src/main/java/org/gridsuite/filter/wip/FilterLoader.java
+++ src/main/java/org/gridsuite/filter/wip/FilterLoader.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
+ * @author Joris Mancini <joris.mancini_externe at rte-france.com>
  */
 public interface FilterLoader {
     List<Filter> getNewFilters(List<UUID> uuids);
